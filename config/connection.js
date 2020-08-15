24a61f651ae9df97f339b4b9b65ced090e6e04de
Dale Foskey <dalefoskey@icloud.com>
2020-08-15 13:00:24 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,29 +1,28 @@
 // * required modules
-require("dotenv").config(); // use environment variables
-const mysql = require("mysql");
-let connection;
+require('dotenv').config() // use environment variables
+const mysql = require('mysql')
+let connection
 
 // * Set up MySQL connection.
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection + mysql.createConnection(process.env.JAWSDB_URL)
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Coding2020",
-    database: "burgers_db",
-  });
+    host: 'localhost',
+    user: 'root',
+    password: 'hacktheplanet',
+    database: 'todoagain_db'
+  })
 }
 
 // * Make connection.
-connection.connect((err) => {
+connection.connect(err => {
   if (err) {
-    console.error(`error connecting: ${err.stack}`);
-    return;
+    console.error(`error connecting: ${err.stack}`)
+    return
   }
-  console.log(`connected as id ${connection.threadId}`);
-});
+  console.log(`connected as id ${connection.threadId}`)
+})
 
 // * Export connection for our ORM to use.
-module.exports = connection;
+module.exports = connection
